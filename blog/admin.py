51a661d45c179a1e774b05d001aa9a5e8cef1cd7
Priyanshu999 <spriyanshuprs@gmail.com>
2021-05-21 15:35:05 +0530

    Almost completed the blog's backend

---
+++ blog/admin.py
@@ -0,0 +1,17 @@
+from operator import pos
+from django.contrib import admin
+from .models import Post, Comment
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title')
+    fieldsets = [
+        (None, { 'fields': [('title','text', 'created_date', 'published_date')] } ),
+    ]
+
+    def save_model(self, request, obj, form, chang):
+        if getattr(obj, 'author', None) is None:
+            obj.author = request.user
+        obj.save()
+
+admin.site.register(Comment)
+admin.site.register(Post, PostAdmin)
